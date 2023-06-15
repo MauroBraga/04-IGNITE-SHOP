44697cede352febe4c6d12203d342996bb4e3cd6
MauroBraga <M@r202150016774>
2023-06-15 10:47:15 -0300

    Estilos Globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
 
+globalStyles()
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
